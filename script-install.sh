c6ede154f21f38035b4fca63a45bd7c00cd4de53
fordpref <mike@ermanfamily.com>
2015-07-10 16:25:51 -0400

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -13,14 +13,17 @@ cd i3
 sudo make 
 sudo make install
 
-sudo apt-get -y install i3lock i3status scrot conky-all nm-applet scrot
+sudo apt-get -y install i3lock i3status scrot conky-all scrot
 
 cd ../vzp-i3-configs
 
 cp .conkyrc ~
 cp .i3-lock ~
+chmod 555 ~/.i3-lock
 cp .start-conky ~
+chmod 555 ~/.start-conky
 cp scrot.sh ~
+chmod 555 ~/scrot.sh
 mkdir ~/.i3
 cp config ~/.i3/
 
@@ -28,8 +31,11 @@ cp config ~/.i3/
 
 sudo cp .conkyrc /root/
 sudo cp .i3-lock /root/
+sudo chmod 555 /root/.i3-lock
 sudo cp .start-conky /root/
+sudo chmod 555 /root/.start-conky
 sudo cp scrot.sh /root/
+sudo chmod 555 /root/scrot.sh
 sudo mkdir /root/.i3
 sudo cp config ~/.i3/
 cd ..
